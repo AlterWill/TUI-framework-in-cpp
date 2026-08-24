d86250005eeb5784fd90cbe043fc0e12911ccb47
AlterWill <rishonk117@gmail.com>
2026-08-24 11:46:48 +0530

    refactor: maded alignment struct separte file and insets use size_t data type

--- include/insets.hpp
+++ include/insets.hpp
@@ -1,26 +1,28 @@
 #pragma once
 
-struct Insets {
-  int top{};
-  int bottom{};
-  int right{};
-  int left{};
+#include <cstddef>
 
-  static constexpr Insets all(int value) {
+struct Insets {
+  std::size_t top{};
+  std::size_t bottom{};
+  std::size_t right{};
+  std::size_t left{};
+
+  static constexpr Insets all(std::size_t value) {
     return {value, value, value, value};
   }
 
-  static constexpr Insets symmetric(int x, int y) { return {y, y, x, x}; }
+  static constexpr Insets symmetric(std::size_t x, std::size_t y) { return {y, y, x, x}; }
 
   // Getters
-  int getTop() const { return top; }
-  int getBottom() const { return bottom; }
-  int getRight() const { return right; }
-  int getLeft() const { return left; }
+  std::size_t getTop() const { return top; }
+  std::size_t getBottom() const { return bottom; }
+  std::size_t getRight() const { return right; }
+  std::size_t getLeft() const { return left; }
 
   // Setters
-  void setTop(int val) { top = val; }
-  void setBottom(int val) { bottom = val; }
-  void setRight(int val) { right = val; }
-  void setLeft(int val) { left = val; }
+  void setTop(std::size_t val) { top = val; }
+  void setBottom(std::size_t val) { bottom = val; }
+  void setRight(std::size_t val) { right = val; }
+  void setLeft(std::size_t val) { left = val; }
 };
