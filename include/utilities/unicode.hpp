43b15925041a827a74a9737138d7a4b2c3b5aa19
AlterWill <rishonk117@gmail.com>
2026-09-12 23:26:01 +0530

    feat(unicode): UTF32 convertion string convertion added

--- include/utilities/unicode.hpp
+++ include/utilities/unicode.hpp
@@ -33,4 +33,49 @@ inline std::string toUtf8(const std::u32string &text) {
   return result;
 }
 
+inline std::u32string toUtf32(const std::string &text) {
+  std::u32string result;
+  std::size_t i = 0;
+  while (i < text.size()) {
+    unsigned char c = static_cast<unsigned char>(text[i]);
+    if (c <= 0x7F) {
+      result.push_back(c);
+      i += 1;
+    } else if ((c & 0xE0) == 0xC0) {
+      if (i + 1 < text.size()) {
+        char32_t cp = static_cast<char32_t>(((c & 0x1F) << 6) |
+                      (static_cast<unsigned char>(text[i + 1]) & 0x3F));
+        result.push_back(cp);
+        i += 2;
+      } else {
+        break;
+      }
+    } else if ((c & 0xF0) == 0xE0) {
+      if (i + 2 < text.size()) {
+        char32_t cp = static_cast<char32_t>(((c & 0x0F) << 12) |
+                      ((static_cast<unsigned char>(text[i + 1]) & 0x3F) << 6) |
+                      (static_cast<unsigned char>(text[i + 2]) & 0x3F));
+        result.push_back(cp);
+        i += 3;
+      } else {
+        break;
+      }
+    } else if ((c & 0xF8) == 0xF0) {
+      if (i + 3 < text.size()) {
+        char32_t cp = static_cast<char32_t>(((c & 0x07) << 18) |
+                      ((static_cast<unsigned char>(text[i + 1]) & 0x3F) << 12) |
+                      ((static_cast<unsigned char>(text[i + 2]) & 0x3F) << 6) |
+                      (static_cast<unsigned char>(text[i + 3]) & 0x3F));
+        result.push_back(cp);
+        i += 4;
+      } else {
+        break;
+      }
+    } else {
+      i += 1;
+    }
+  }
+  return result;
+}
+
 } // namespace unicode
