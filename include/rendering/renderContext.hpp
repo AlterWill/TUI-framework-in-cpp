e0c48a2583a07843e9138d61705bef1e49c1e69c
AlterWill <rishonk117@gmail.com>
2026-09-14 13:25:59 +0530

    feat(renderContext): Added clipping

--- include/rendering/renderContext.hpp
+++ include/rendering/renderContext.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "utilities/Rect.hpp"
 #include "rendering/buffer.hpp"
 #include "rendering/cell.hpp"
 #include "styling/style.hpp"
+#include "utilities/Rect.hpp"
 
 struct RenderContext {
  private:
@@ -16,13 +16,24 @@ struct RenderContext {
   void setRect(const Rect& r) { currentRect = r; }
   const Rect& getRect() const { return currentRect; }
 
-  void setCell(std::size_t x, std::size_t y, const Cell& cell) { frameBuffer.setCell(x, y, cell); }
+  void setCell(std::size_t x, std::size_t y, const Cell& cell) {
+    if (!currentRect.contains(x, y)) return;
+    frameBuffer.setCell(x, y, cell);
+  }
 
-  void setCell(Point point, const Cell& cell) { frameBuffer.setCell(point, cell); }
+  void setCell(Point point, const Cell& cell) {
+    setCell(point.getX(), point.getY(), cell);
+  }
 
-  void setGlyph(std::size_t x, std::size_t y, char32_t glyph) { frameBuffer.setGlyph(x, y, glyph); }
+  void setGlyph(std::size_t x, std::size_t y, char32_t glyph) {
+    if (!currentRect.contains(x, y)) return;
+    frameBuffer.setGlyph(x, y, glyph);
+  }
 
-  void setStyle(std::size_t x, std::size_t y, Style style) { frameBuffer.setStyle(x, y, style); }
+  void setStyle(std::size_t x, std::size_t y, Style style) {
+    if (!currentRect.contains(x, y)) return;
+    frameBuffer.setStyle(x, y, style);
+  }
 
   const Cell& at(std::size_t x, std::size_t y) const { return frameBuffer.at(x, y); }
 
@@ -30,3 +41,5 @@ struct RenderContext {
 
   const Buffer& getBuffer() const { return frameBuffer; }
 };
+
+
